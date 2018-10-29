42d809438993e31049340e1393b109cec3194d26
Paris <bobby.e.paris@accenture.com>
2018-10-29 13:00:53 +0800

    OR: added BaseTest

--- src/main/test/com/orangehrmlive/opensource/steps/BaseTest.java
+++ src/main/test/com/orangehrmlive/opensource/steps/BaseTest.java
@@ -4,21 +4,32 @@ import cucumber.api.java.Before;
 import org.fluentlenium.adapter.junit.FluentTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class BaseTest extends FluentTest {
+    private String sp = File.separator;
+    private String propertyPath = System.getProperty("user.dir") + sp + "src" + sp + "main" + sp + "resources" + sp + "properties" + sp;
 
     @Before("UISetup")
     public void before() {
-
+        Properties prop = new Properties();
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        InputStream stream = loader.getResourceAsStream(propertyPath + "ui.properties");
+        try {
+            prop.load(stream);
+            getDriver().get(prop.getProperty("orange.url"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public WebDriver newWebDriver() {
-        String sp = File.separator;
+
         String driverPath = System.getProperty("user.dir") + sp + "src" + sp + "main" + sp + "resources" + sp + "drivers" + sp;
 
         System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver.exe"); // Set for ChromeDriver
@@ -36,5 +47,10 @@ public class BaseTest extends FluentTest {
 
     public String getCapa() {
         return "";
+    }
+
+    @Before("UISetup")
+    public void after(){
+        getDriver().close();
     }
 }
--- src/main/test/com/orangehrmlive/opensource/steps/LoginStepDef.java
+++ src/main/test/com/orangehrmlive/opensource/steps/LoginStepDef.java
@@ -1,6 +1,8 @@
 package com.orangehrmlive.opensource.steps;
 
 import com.orangehrmlive.opensource.pages.LoginPage;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import org.fluentlenium.core.annotation.Page;
@@ -8,6 +10,11 @@ import org.fluentlenium.core.annotation.Page;
 public class LoginStepDef extends BaseTest {
     @Page
     private LoginPage loginPage;
+
+    @Before("@UISetup")
+    public void before() {
+        super.before();
+    }
 
     @Given("^Orange HRM login page is displayed$")
     public void diplayLoginPage() {
@@ -19,4 +26,8 @@ public class LoginStepDef extends BaseTest {
         loginPage.login();
     }
 
+    @After("@UISetup")
+    public void after() {
+        super.after();
+    }
 }
