fad36fa74f3ff264963a1d979257ae80232ba001
Paris <bobby.e.paris@accenture.com>
2018-10-29 12:42:21 +0800

    OR: added BaseTest

--- src/main/test/com/orangehrmlive/opensource/steps/BaseTest.java
+++ src/main/test/com/orangehrmlive/opensource/steps/BaseTest.java
@@ -1,8 +1,25 @@
 package com.orangehrmlive.opensource.steps;
 
+import cucumber.api.java.Before;
 import org.fluentlenium.adapter.junit.FluentTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BaseTest extends FluentTest {
+
+    @Before("UISetup")
+    public void before() {
+
+    }
+
+    @Override
+    public WebDriver newWebDriver() {
+        System.setProperty("webdriver.chrome.driver", "C:/chromeDriver/chromedriver.exe"); // Set for ChromeDriver
+        WebDriver driver = new ChromeDriver();
+        return driver;
+    }
 
     public String getBrowser() {
         return "";
